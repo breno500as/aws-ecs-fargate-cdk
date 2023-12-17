b458083c8560c0ac6d632e1d795a06669a9109f6
breno.mcosta <breno.mcosta@pbh.gov.br>
2023-12-17 16:52:24 -0300

    update

---
+++ src/main/java/com/myorg/Alb02Stack.java
@@ -0,0 +1,86 @@
+package com.myorg;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import software.amazon.awscdk.Duration;
+import software.amazon.awscdk.RemovalPolicy;
+import software.amazon.awscdk.Stack;
+import software.amazon.awscdk.StackProps;
+import software.amazon.awscdk.services.applicationautoscaling.EnableScalingProps;
+import software.amazon.awscdk.services.ecs.AwsLogDriverProps;
+import software.amazon.awscdk.services.ecs.Cluster;
+import software.amazon.awscdk.services.ecs.ContainerImage;
+import software.amazon.awscdk.services.ecs.CpuUtilizationScalingProps;
+import software.amazon.awscdk.services.ecs.LogDriver;
+import software.amazon.awscdk.services.ecs.ScalableTaskCount;
+import software.amazon.awscdk.services.ecs.patterns.ApplicationLoadBalancedFargateService;
+import software.amazon.awscdk.services.ecs.patterns.ApplicationLoadBalancedTaskImageOptions;
+import software.amazon.awscdk.services.elasticloadbalancingv2.HealthCheck;
+import software.amazon.awscdk.services.events.targets.SnsTopic;
+import software.amazon.awscdk.services.logs.LogGroup;
+import software.constructs.Construct;
+
+public class Alb02Stack extends Stack {
+	
+	  public Alb02Stack(final Construct scope, final String id, Cluster cluster, SnsTopic productTopic) {
+	        this(scope, id, null, cluster, productTopic);
+	    }
+
+	    public Alb02Stack(final Construct scope, final String id, final StackProps props, Cluster cluster, SnsTopic productTopic) {
+	        super(scope, id, props);
+
+ 
+
+ 
+	        Map<String, String> envVariables = new HashMap<>();
+	        envVariables.put("AWS_REGION", "us-east-1");
+	   
+
+
+	        ApplicationLoadBalancedFargateService service02 = ApplicationLoadBalancedFargateService.Builder.create(this, "ALB02")
+	                .serviceName("alb-02")
+	                .cluster(cluster)
+	                .cpu(512)
+	                .memoryLimitMiB(1024)
+	                .desiredCount(2)
+	                .listenerPort(9090)
+	                .taskImageOptions(
+	                        ApplicationLoadBalancedTaskImageOptions.builder()
+	                                .containerName("aws_project02")
+	                                .image(ContainerImage.fromRegistry("breno500as/aws-listener:1.1.0"))
+	                                .containerPort(9090)
+	                                .logDriver(LogDriver.awsLogs(AwsLogDriverProps.builder()
+	                                        .logGroup(LogGroup.Builder.create(this, "Service02LogGroup")
+	                                                .logGroupName("Service02")
+	                                                .removalPolicy(RemovalPolicy.DESTROY)
+	                                                .build())
+	                                        .streamPrefix("Service02")
+	                                        .build()))
+	                                .environment(envVariables)
+	                                .build())
+	                .publicLoadBalancer(true)
+	                .build();
+
+	        service02.getTargetGroup().configureHealthCheck(new HealthCheck.Builder()
+	                .path("/actuator/health")
+	                .port("9090")
+	                .healthyHttpCodes("200")
+	                .build());
+
+	        ScalableTaskCount scalableTaskCount = service02.getService().autoScaleTaskCount(EnableScalingProps.builder()
+	                .minCapacity(2)
+	                .maxCapacity(4)
+	                .build());
+
+	        scalableTaskCount.scaleOnCpuUtilization("Alb02AutoScalling", CpuUtilizationScalingProps.builder()
+	                .targetUtilizationPercent(50)
+	                .scaleInCooldown(Duration.seconds(60))
+	                .scaleOutCooldown(Duration.seconds(60))
+	                .build());
+
+	        
+	      
+	    }
+
+}
---
+++ src/main/java/com/myorg/AwsEcsFargateCdkApp.java
@@ -0,0 +1,32 @@
+package com.myorg;
+
+import software.amazon.awscdk.App;
+
+public class AwsEcsFargateCdkApp {
+	public static void main(final String[] args) {
+		App app = new App();
+
+		final VpcStack vpcStack = new VpcStack(app, "Vpc");
+
+		final ClusterStack clusterStack = new ClusterStack(app, "Cluster", vpcStack.getVpc());
+		clusterStack.addDependency(vpcStack);
+
+		final RdsStack rdsStack = new RdsStack(app, "Rds", vpcStack.getVpc());
+		rdsStack.addDependency(vpcStack);
+
+		final SnsStack snsStack = new SnsStack(app, "Sns");
+
+		final Alb01Stack alb01Stack = new Alb01Stack(app, "Alb01", clusterStack.getCluster(),
+				snsStack.getProductTopic());
+		alb01Stack.addDependency(clusterStack);
+		alb01Stack.addDependency(rdsStack);
+		alb01Stack.addDependency(snsStack);
+
+		final Alb02Stack service02Stack = new Alb02Stack(app, "Alb02", clusterStack.getCluster(),
+				snsStack.getProductTopic());
+		service02Stack.addDependency(clusterStack);
+		service02Stack.addDependency(snsStack);
+
+		app.synth();
+	}
+}
