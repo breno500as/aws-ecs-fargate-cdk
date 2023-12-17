73319f68e4104a1c56a2a02853d8553b7d1fe2c7
breno.mcosta <breno.mcosta@pbh.gov.br>
2023-12-17 18:06:30 -0300

    a

--- src/main/java/com/myorg/Alb02Stack.java
+++ src/main/java/com/myorg/Alb02Stack.java
@@ -19,6 +19,10 @@ import software.amazon.awscdk.services.ecs.patterns.ApplicationLoadBalancedTaskI
 import software.amazon.awscdk.services.elasticloadbalancingv2.HealthCheck;
 import software.amazon.awscdk.services.events.targets.SnsTopic;
 import software.amazon.awscdk.services.logs.LogGroup;
+import software.amazon.awscdk.services.sns.subscriptions.SqsSubscription;
+import software.amazon.awscdk.services.sqs.DeadLetterQueue;
+import software.amazon.awscdk.services.sqs.Queue;
+import software.amazon.awscdk.services.sqs.QueueEncryption;
 import software.constructs.Construct;
 
 public class Alb02Stack extends Stack {
@@ -29,14 +33,34 @@ public class Alb02Stack extends Stack {
 
 	    public Alb02Stack(final Construct scope, final String id, final StackProps props, Cluster cluster, SnsTopic productTopic) {
 	        super(scope, id, props);
-
  
+	       final Queue productDlqQueue = Queue.Builder.create(this, "ProductDlqQueue")
+	    		   .queueName("product-dlq-queue")
+	    		   .enforceSsl(false)
+	    	       .encryption(QueueEncryption.UNENCRYPTED)
+	    		   .build();
+	       
+	       final DeadLetterQueue deadLetterQueue = DeadLetterQueue.builder()
+	                .queue(productDlqQueue)
+	                .maxReceiveCount(3)
+	                .build();
+	       
+	       
+	       final Queue productQueue = Queue.Builder.create(this, "ProductQueue")
+	                .queueName("product-queue")
+	                .enforceSsl(false)
+	                .encryption(QueueEncryption.UNENCRYPTED)
+	                .deadLetterQueue(deadLetterQueue)
+	                .build();
+	       
+	       final SqsSubscription sqsSubscription = SqsSubscription.Builder.create(productQueue).build();
+	       productTopic.getTopic().addSubscription(sqsSubscription);
+	       
+	       Map<String, String> envVariables = new HashMap<>();
+	       envVariables.put("AWS_REGION", "us-east-1");
+	       envVariables.put("AWS_SQS_QUEUE_PRODUCT_NAME", productQueue.getQueueName());
 
- 
-	        Map<String, String> envVariables = new HashMap<>();
-	        envVariables.put("AWS_REGION", "us-east-1");
 	   
-
 
 	        ApplicationLoadBalancedFargateService service02 = ApplicationLoadBalancedFargateService.Builder.create(this, "ALB02")
 	                .serviceName("alb-02")
